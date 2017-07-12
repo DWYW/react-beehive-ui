bbe2fcc1d6f52b8120ace56df62e038820a32bcb
dwyw <2545072150@qq.com>
2017-07-12 21:20:56 +0800

    navtree

--- src/components/navigation/NavTree.js
+++ src/components/navigation/NavTree.js
@@ -19,42 +19,46 @@ class NavTree extends React.Component {
    }
 
    _init() {
+
       this._hashPath = location.hash.substring(1);
       let data = this._initData(this.props.data);
-      data = this._initNodeAttribute(data);
+      data = this._initNodeAttribute(data, "isOpen", true);
       console.log(data)
       this.setState({
          data: data
       })
 
-      data[0]['isSelected'] = true;
-      this.nodeSelected = {
-         layer: "",
-         key: 0
-      };
-      this._route = {
-         layer: "",
-         key: 0
-      }
+      // this.nodeSelected = {
+      //    layer: "",
+      //    key: 0
+      // };
+      // this._route = {
+      //    layer: "",
+      //    key: 0
+      // }
    }
 
-   _initNodeAttribute(nodes) {
+   _initNodeAttribute(nodes, attribute, value) {
       let posArr = this._nodeSelectedPos.split("-");
-      posArr = posArr.slice(1, posArr.length-1);
+      posArr = posArr.slice(1, posArr.length - 1);
 
       if(posArr.length == 0) {
+         this._nodeOpend = "0"
          return nodes;
       }
 
       let treeNode = nodes;
+      posArr.forEach((item, key) => {
+         // if(key == (posArr.length - 1)) {
+         //    this._nodeOpened = treeNode[parseInt(item)]._pos;
+         // }
 
-      posArr.forEach((item) => {
          if(treeNode[parseInt(item)].children && treeNode[parseInt(item)].children.length > 0) {
-            treeNode[parseInt(item)].isOpen = true;
+            treeNode[parseInt(item)][attribute] = value;
             treeNode = treeNode[parseInt(item)].children;
          }
       })
-
+      // console.log(this._nodeOpened);
       return nodes
    }
 
@@ -69,6 +73,15 @@ class NavTree extends React.Component {
          if(node.to && node.to.pathname == this._hashPath) {
             node.isSelected = true;
             this._nodeSelectedPos = node._pos;
+            // this.nodeSelected = {
+            //    layer: pos ? pos : '0',
+            //    key: key
+            // };
+            // this._route = {
+            //    layer: pos ? pos : '0',
+            //    key: key
+            // }
+            console.log(node._pos);
          }
 
          if(node.children && node.children instanceof Array && node.children.length > 0) {
@@ -79,10 +92,13 @@ class NavTree extends React.Component {
       return nodes
    }
 
-   _setNodeSelect(layerPath, key, attr) {
+   _setNodeSelect(pos, attr) {
       const data = this.state.data;
+      const posArr = pos.split("-");
+      let layerPath = posArr.slice(1, posArr.length-1);
+      let key = posArr.slice(-1);
 
-      if(layerPath == 'root') {
+      if(layerPath.length == 0) {
          data[key][attr] = !data[key][attr];
       }
       else {
@@ -96,80 +112,83 @@ class NavTree extends React.Component {
       return data
    }
 
-   _getLayerPath(layer){
-      if(layer.toString() !== ""){
-         return layer.toString().split("-");
-      }
-      else {
-         return "root"
-      }
+   _getLayerPath(pos){
+      // if(layer.toString() !== ""){
+      //    return layer.toString().split("-");
+      // }
+      // else {
+      //    return "root"
+      // }
    }
 
-   _getKeyValue(layerPath, key, attr) {
-      const data = this.state.data;
+   _getKeyValue(pos, attr) {
+      console.log(pos, attr)
+      // const data = this.state.data;
 
-      if(layerPath == 'root') {
-         return data[key][attr];
-      }
-      else {
-         let children = data;
-         layerPath.map((path) => {
-            children = children[parseInt(path)].children;
-         })
-         return children[key][attr];
-      }
+      // if(layerPath == 'root') {
+      //    return data[key][attr];
+      // }
+      // else {
+      //    let children = data;
+      //    layerPath.map((path) => {
+      //       children = children[parseInt(path)].children;
+      //    })
+      //    return children[key][attr];
+      // }
    }
 
-   _handleClick(layer, key, node) {
+   _handleClick(node) {
+      const poses = node._pos.split("-");
       let data = this.state.data;
-      const layerPath = this._getLayerPath(layer);
+      // const layerPath = this._getLayerPath(layer);
+      // console.log(layerPath)
 
       if(node.children && node.children.length > 0) {
-         if(this.nodeOpen && (this.nodeOpen.layer != layer || this.nodeOpen.key != key)){
-            if(this._getKeyValue(this._getLayerPath(this.nodeOpen.layer), this.nodeOpen.key, IS_OPEN)){
-               data = this._setNodeSelect(this._getLayerPath(this.nodeOpen.layer), this.nodeOpen.key, IS_OPEN);
-            }
-         }
+         // if(this.nodeOpen && (this.nodeOpen.layer != layer || this.nodeOpen.key != key)){
+         //    if(this._getKeyValue(this._getLayerPath(this.nodeOpen.layer), this.nodeOpen.key, IS_OPEN)){
+         //       data = this._setNodeSelect(this._getLayerPath(this.nodeOpen.layer), this.nodeOpen.key, IS_OPEN);
+         //    }
+         // }
 
-         data = this._setNodeSelect(layerPath, key, IS_OPEN);
-         this.nodeOpen = {
-            layer: layer,
-            key: key
-         }
-      }
-
-      if(this.nodeSelected) {
-         if(this.nodeSelected.layer != layer || this.nodeSelected.key != key) {
-            if(this._getKeyValue(this._getLayerPath(this.nodeSelected.layer), this.nodeSelected.key, IS_SELECTED)) {
-               data = this._setNodeSelect(this._getLayerPath(this.nodeSelected.layer), this.nodeSelected.key, IS_SELECTED);
-            }
-            data = this._setNodeSelect(layerPath, key, IS_SELECTED);
-            this.nodeSelected = {
-               layer: layer,
-               key: key
-            };
-            if(node.children && node.children.length == 0){
-               this._route = {
-                  layer: layer,
-                  key: key
-               }
-            }
-
-            // if(!node.children || node.children.length == 0){
-            //   //callback
-            // }
-         }
-      }
-      else {
-         data = this._setNodeSelect(layerPath, key, IS_SELECTED);
-         this.nodeSelected = {
-            layer: layer,
-            key: key
-         };
-         // if(!node.children || node.children.length == 0){
-         //    //callback
+         data = this._setNodeSelect(node._pos, IS_OPEN);
+         // this.nodeOpen = {
+         //    layer: layer,
+         //    key: key
          // }
       }
+
+      // if(this.nodeSelected) {
+      //    if(this.nodeSelected.layer != layer || this.nodeSelected.key != key) {
+      //       if(this._getKeyValue(this._getLayerPath(this.nodeSelected.layer), this.nodeSelected.key, IS_SELECTED)) {
+      //          data = this._setNodeSelect(this._getLayerPath(this.nodeSelected.layer), this.nodeSelected.key, IS_SELECTED);
+      //       }
+      //       data = this._setNodeSelect(layerPath, key, IS_SELECTED);
+      //       this.nodeSelected = {
+      //          layer: layer,
+      //          key: key
+      //       };
+      //       if(node.children && node.children.length == 0){
+      //          this._route = {
+      //             layer: layer,
+      //             key: key
+      //          }
+      //       }
+
+      //       // if(!node.children || node.children.length == 0){
+      //       //   //callback
+      //       // }
+      //    }
+      // }
+      // else {
+      //    data = this._setNodeSelect(layerPath, key, IS_SELECTED);
+      //    this.nodeSelected = {
+      //       layer: layer,
+      //       key: key
+      //    };
+      //    // if(!node.children || node.children.length == 0){
+      //    //    //callback
+      //    // }
+      // }
 
       this.setState({
          data: data
@@ -179,27 +198,27 @@ class NavTree extends React.Component {
    render() {
       let {data, className, spacing} = this.props;
       data = this.state.data;
-      const layer = "";
+      // const layer = "";
       className = className ? `nav-tree ${className}` : 'nav-tree';
 
-      const createTreeNode = (treeNodes, spacing, layer) => {
+      const createTreeNode = (treeNodes, spacing) => {
          spacing == undefined ? spacing = 16 : spacing += 16;
          return (
             treeNodes.map((item, key) => {
                const iconClassName = `iconfont ${item.iconClassName}`;
-               const itemLayer = layer == "" ? `${key}` : `${layer}-${key}`;
+               // const itemLayer = layer == "" ? `${key}` : `${layer}-${key}`;
                const itemStyle = {
                   height: item.isOpen == false ? "0px" : `${42 * item.children.length}px`
                };
 
                return (
                   <li key={item.label} className="nav-tree-container">
-                     {!item.isSelected && (this._route.layer != layer || this._route.key != key) && item.children.length == 0 ?
+                     {item.to && item._pos !== this._nodeSelectedPos ?
                         (
                            <Link className="nav-tree-container-label"
                               style={{paddingLeft: `${spacing}px`}} data-selected={item.isSelected}
                               to={item.to}
-                              onClick={() => this._handleClick(layer, key, item)}>
+                              onClick={() => this._handleClick(item)}>
                               {item.iconClassName &&
                                  <i className={iconClassName}></i>
                               }
@@ -207,7 +226,7 @@ class NavTree extends React.Component {
                            </Link>
                         ) : (
                            <div className="nav-tree-container-label" style={{paddingLeft: `${spacing}px`}} data-selected={item.isSelected}
-                              onClick={() => this._handleClick(layer, key, item)}
+                              onClick={() => this._handleClick(item)}
                            >
                               {item.iconClassName &&
                                  <i className={iconClassName}></i>
@@ -224,7 +243,7 @@ class NavTree extends React.Component {
                      <div className="nav-tree-children-container">
                         {item.children && (item.children instanceof Array) && item.children.length > 0 &&
                            <ul className={className} style={itemStyle}>
-                              {createTreeNode(item.children, spacing, itemLayer)}
+                              {createTreeNode(item.children, spacing)}
                            </ul>
                         }
                      </div>
@@ -236,7 +255,7 @@ class NavTree extends React.Component {
 
       return (
          <ul className={className}>
-            {createTreeNode(data, spacing, layer)}
+            {createTreeNode(data, spacing)}
          </ul>
       )
 
